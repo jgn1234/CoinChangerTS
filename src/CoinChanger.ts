9b4be26d94078b9da2654bc171dfde360db7804f
jgn1234 <60579482+jgn1234@users.noreply.github.com>
2020-07-03 18:09:44 -0400

    Refactor

--- src/CoinChanger.ts
+++ src/CoinChanger.ts
@@ -5,6 +5,13 @@ type ChangeTracker = {
   coinsToReturn: string,
 }
 
+const Penny = {coin: 'P', value: 1}
+const Nickel = {coin: 'N', value: 5}
+const Dime = {coin: 'D', value: 10}
+const Quarter = {coin: 'Q', value: 25}
+const Fifty = {coin: 'F', value: 50}
+const Dollar = {coin: 'S', value: 100}
+
 const makeChange = (acc : ChangeTracker, currentCoin : any, ) => {
   let {remainingChange, coinsToReturn} = acc
   while(remainingChange >= currentCoin.value) {
@@ -15,12 +22,7 @@ const makeChange = (acc : ChangeTracker, currentCoin : any, ) => {
 }
 
 const coinChanger = (changeToMake: number) => {
-  const coins = [
-    {coin:'Q', value: 25},
-    {coin:'D', value: 10},
-    {coin:'N', value: 5},
-    {coin:'P', value: 1},
-  ]
+  const coins = [ Quarter, Dime, Nickel, Penny ]
   
   const res = coins.reduce(makeChange, <ChangeTracker> {remainingChange: changeToMake, coinsToReturn: ''})
   return res.coinsToReturn
